5515c4b1555c21297b80fa749eb33ccf8e6dff18
Md Mahtab Uddin <mahtab.sentu@outlook.com>
2021-10-14 22:26:08 +0600

    initialize react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter , Switch, Route} from 'react-router-dom';
+import Home from './Component/Home/Home'
+import Header from './Component/Header/Header'
+import Services from './Component/Services/Services'
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+      <Header></Header>
+      <Switch>
+        <Route path="/home">
+<Home></Home>
+        </Route>
+
+        <Route path="/">
+        <Home></Home>
+
+        </Route>
+<Route path="/services">
+<Services></Services>
+</Route>
+      </Switch>
       
+      </BrowserRouter>
     </div>
   );
 }
--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
-            <h1>header</h1>
+            <NavLink>
+            <Link to="/home"></Link>
+            <Link to="/services"></Link>
+
+            </NavLink>
+            
         </div>
     );
 };
